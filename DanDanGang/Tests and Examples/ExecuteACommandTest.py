b6706c5be51fb1f3e4d7801b4f352c4325415ed6
Dan <djh242@cam.ac.uk>
2019-01-31 12:33:29 +0000

    Trying some basic SQL tests

--- DanDanGang/Tests and Examples/ExecuteACommandTest.py
+++ DanDanGang/Tests and Examples/ExecuteACommandTest.py
@@ -3,10 +3,13 @@ import bayeslite
 db_pathname = 'foo.bdb'
 
 result = None
-with bayeslite.bayesdb_open(pathname='foo.bdb') as bdb:
-    result = bdb.execute('SELECT 42')
+with bayeslite.bayesdb_open(pathname=db_pathname) as bdb:
+    bdb.execute("DROP TABLE testTable(name TEXT, x INT, y INT)")
+    bdb.sql_execute("INSERT INTO testTable VALUES('Alice', 1, 11)")
+    bdb.sql_execute("INSERT INTO testTable VALUES('Bob', 2, 12)")
+    bdb.sql_execute("INSERT INTO testTable VALUES('Candice', 3, 13)")
+    result = bdb.execute("SELECT * FROM testTable WHERE name='Alice'")
 
-if result is not None:
-    print result
-else:
-    print "failure"
+print result
+for i in result:
+    print(i)
