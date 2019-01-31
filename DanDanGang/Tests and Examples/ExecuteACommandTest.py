bdc6f5f80ec1d3d6066f4ab3ed2806c4afcbf28c
Dan <djh242@cam.ac.uk>
2019-01-31 12:49:53 +0000

    ignore me

--- DanDanGang/Tests and Examples/ExecuteACommandTest.py
+++ DanDanGang/Tests and Examples/ExecuteACommandTest.py
@@ -4,7 +4,7 @@ db_pathname = 'foo.bdb'
 
 result = None
 with bayeslite.bayesdb_open(pathname=db_pathname) as bdb:
-    bdb.execute("DROP TABLE testTable(name TEXT, x INT, y INT)")
+    bdb.execute("DROP TABLE testTable")
     bdb.sql_execute("INSERT INTO testTable VALUES('Alice', 1, 11)")
     bdb.sql_execute("INSERT INTO testTable VALUES('Bob', 2, 12)")
     bdb.sql_execute("INSERT INTO testTable VALUES('Candice', 3, 13)")
