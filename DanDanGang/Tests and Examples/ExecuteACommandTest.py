f7333d9e6fbec6c0011e32905cd04900484e66fe
Dan <djh242@cam.ac.uk>
2019-02-01 20:18:48 +0000

    eliminating sql

--- DanDanGang/Tests and Examples/ExecuteACommandTest.py
+++ DanDanGang/Tests and Examples/ExecuteACommandTest.py
@@ -5,10 +5,10 @@ db_pathname = 'foo.bdb'
 
 with bayeslite.bayesdb_open(pathname=db_pathname) as bdb:
     bdb.execute("DROP TABLE IF EXISTS testTable;")
-    bdb.sql_execute('CREATE TABLE testTable(name TEXT, x INT, y INT);')
-    bdb.sql_execute("INSERT INTO testTable VALUES('Alice', 1, 11);")
-    bdb.sql_execute("INSERT INTO testTable VALUES('Bob', 2, 12);")
-    bdb.sql_execute("INSERT INTO testTable VALUES('Candice', 3, 13);")
+    bdb.execute('CREATE TABLE testTable(name TEXT, x INT, y INT);')
+    bdb.execute("INSERT INTO testTable VALUES('Alice', 1, 11);")
+    bdb.execute("INSERT INTO testTable VALUES('Bob', 2, 12);")
+    bdb.execute("INSERT INTO testTable VALUES('Candice', 3, 13);")
     result = bdb.execute("SELECT * FROM testTable WHERE name='Alice';")
     for i in result:
         print i
