ffa375d244fd85843aac95953e0e22c0dd2a06e5
Dan <djh242@cam.ac.uk>
2019-02-01 20:18:13 +0000

    D:

--- DanDanGang/Tests and Examples/ExecuteACommandTest.py
+++ DanDanGang/Tests and Examples/ExecuteACommandTest.py
@@ -12,14 +12,14 @@ with bayeslite.bayesdb_open(pathname=db_pathname) as bdb:
     result = bdb.execute("SELECT * FROM testTable WHERE name='Alice';")
     for i in result:
         print i
-    print()
+    print "\n"
 
     pop = bdb.execute("CREATE POPULATION testPopulation FOR testTable WITH SCHEMA (SET STATTYPES OF x,y TO numerical, numerical; IGNORE name;);")
     result = bdb.execute("SIMULATE y FROM testPopulation GIVEN x=2 LIMIT 20")
     print result
-    print
+    print "\n"
 
     result = bdb.execute("SELECT * FROM testTable WHERE name='Alice';")
     for i in result:
         print i
-    print
+    print "\n"
