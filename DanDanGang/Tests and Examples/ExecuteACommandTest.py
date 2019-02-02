8b2eeb0891167e13e421da567df8210fe614171e
Dan <djh242@cam.ac.uk>
2019-02-02 17:01:38 +0000

    streamlined test to demo simulate not working

--- DanDanGang/Tests and Examples/ExecuteACommandTest.py
+++ DanDanGang/Tests and Examples/ExecuteACommandTest.py
@@ -1,29 +1,29 @@
-import bayeslite
-
+import bayeslite, random
 
 db_pathname = 'foo.bdb'
 
 with bayeslite.bayesdb_open(pathname=db_pathname) as bdb:
     # Create TestTable
+    bdb.execute("DROP POPULATION IF EXISTS testPopulation;")
     bdb.execute("DROP TABLE IF EXISTS testTable;")
     bdb.sql_execute('CREATE TABLE testTable(name TEXT, x INT, y INT);')
-    # Fill it with some data
-    bdb.sql_execute("INSERT INTO testTable VALUES('Alice', 1, 11);")
-    bdb.sql_execute("INSERT INTO testTable VALUES('Bob', 2, 12);")
-    bdb.sql_execute("INSERT INTO testTable VALUES('Candice', 3, 13);")
 
-    # Print out Alice's row
-    result = bdb.execute("SELECT * FROM testTable WHERE name='Alice';")
-    for i in result:
-        print "Result of the first select: "+str(i)+"\n"
+    # Fill it with some data
+    for i in range(100):
+        name = random.choice(['A', 'B', 'C'])
+        x = random.randint(1, 5)
+        y = x + 8 + random.randint(1, 3)
+        bdb.sql_execute("INSERT INTO testTable VALUES(?, ?, ?);", (name, x, y))
 
     # Create a population with which to do a simulation
-    pop = bdb.execute("CREATE POPULATION testPopulation FOR testTable WITH SCHEMA (SET STATTYPES OF x,y TO numerical, numerical; IGNORE name;);")  # Crashes on this query D:
-    result = bdb.execute("SIMULATE y FROM testPopulation GIVEN x=2 LIMIT 20;")
+    pop = bdb.execute(
+        "CREATE POPULATION IF NOT EXISTS testPopulation FOR testTable WITH SCHEMA (MODEL x,y AS NUMERICAL; IGNORE name;);")
+    result = bdb.execute(
+        "SIMULATE y FROM testPopulation GIVEN x=2 LIMIT 20;")  # TODO: This returns an empty cursor, but shouldn't
     print result
-    print "\n"
-
-    # Print out Alice's row again
-    result = bdb.execute("SELECT * FROM testTable WHERE name='Alice';")
     for i in result:
-        print "Result of the first select: "+str(i)+"\n"
+        print i
+
+
+
+
