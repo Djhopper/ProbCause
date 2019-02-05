2cd5e19b5b61aa46ad35a9fb0c25dfb5d6ddb2eb
Dan <djh242@cam.ac.uk>
2019-02-05 13:19:43 +0000

    fixed file->csv test to work without using url

--- DanDanGang/Scripts and Utilities/test_csv_import.py
+++ DanDanGang/Scripts and Utilities/test_csv_import.py
@@ -1,10 +1,10 @@
 from create_table_from_csv import table_from_url, table_from_csv
-import urllib
 import bayeslite
 
 table_name = 'testTable'
 db_pathname = 'foo.bdb'
 url = 'https://query.data.world/s/3c26afdi4kinjoqe3idolsxxk2ph4b'  # from data.world
+file_name = 'wc_forecasts.csv'
 
 print "Test 1:"
 with bayeslite.bayesdb_open(pathname=db_pathname) as bdb:
@@ -16,7 +16,7 @@ with bayeslite.bayesdb_open(pathname=db_pathname) as bdb:
 
 print "Test 2:"
 with bayeslite.bayesdb_open(pathname=db_pathname) as bdb:
-    with urllib.urlopen(url) as f:
+    with open(file_name, 'r') as f:
         table_from_csv(bdb, table_name, f)
         result = bdb.execute('SELECT * FROM testTable LIMIT 3;')
         for i in result:
