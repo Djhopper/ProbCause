73f3dc83ea2e5d110949023bf83f9ab87faca5d1
Dan <djh242@cam.ac.uk>
2019-02-05 18:45:53 +0000

    minor fixes to conv_cursor_to_json

--- DanDanGang/Scripts and Utilities/conv_cursor_to_json.py
+++ DanDanGang/Scripts and Utilities/conv_cursor_to_json.py
@@ -4,8 +4,14 @@ import bayeslite
 
 def conv_cursor_to_json(cursor):
     d = {}
+    print "fetchall:"
+    print cursor.fetchall()
     for row in cursor.fetchall():
+        print "row:"
+        print row
         for i, column in enumerate(row):
+            print "desc:"
+            print cursor.description[i][0]
             d[cursor.description[i][0]] = column
     print "d:"
     print d
