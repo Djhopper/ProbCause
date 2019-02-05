7f96e849ea62e54909ef3585f0c231dd0e83696c
Dan <djh242@cam.ac.uk>
2019-02-05 13:06:35 +0000

    updated a thing to python 2

--- DanDanGang/Scripts and Utilities/create_table_from_csv.py
+++ DanDanGang/Scripts and Utilities/create_table_from_csv.py
@@ -1,6 +1,6 @@
 from bayeslite import read_pandas, read_csv
 import pandas as pd
-import io
+import StringIO
 import requests
 
 
@@ -10,6 +10,6 @@ def table_from_csv(bdb, table_name, f):
 
 def table_from_url(bdb, table_name, url):
     s = requests.get(url).content
-    df = pd.read_csv(io.StringIO(s.decode('utf-8')))
+    df = pd.read_csv(StringIO.StringIO(s.decode('utf-8')))
     print df.index
     read_pandas.bayesdb_read_pandas_df(bdb, table_name, df, create=True, ifnotexists=True)
