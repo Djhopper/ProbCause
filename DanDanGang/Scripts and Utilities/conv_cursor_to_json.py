12a4c5a2946bbb6aaa94fd903658ffce253bd0fe
Dan <djh242@cam.ac.uk>
2019-02-05 18:31:15 +0000

    minor fixes to conv_cursor_to_json

--- DanDanGang/Scripts and Utilities/conv_cursor_to_json.py
+++ DanDanGang/Scripts and Utilities/conv_cursor_to_json.py
@@ -6,3 +6,4 @@ def conv_cursor_to_json(cursor):
     r = [(dict((cursor.description[i][0], value))
           for i, value in enumerate(row)) for row in cursor.fetchall()]
     json_output = json.dumps(r)
+    return json_output
--- DanDanGang/Scripts and Utilities/test_conv_cursor_to_json.py
+++ DanDanGang/Scripts and Utilities/test_conv_cursor_to_json.py
@@ -1,18 +1,18 @@
-import json
 import bayeslite
+from conv_cursor_to_json import conv_cursor_to_json
 
 db_pathname = 'foo.bdb'
 
 
 with bayeslite.bayesdb_open(pathname=db_pathname) as bdb:
-    # Create TestTable
+    # Create and populate table
     bdb.sql_execute('CREATE TABLE t(name TEXT, x INT, y INT);')
-
-    # Fill it with some data
     for name, x, y in [['A', 1, 11], ['B', 2, 12], ['C', 3, 13]]:
         bdb.sql_execute("INSERT INTO t VALUES(?, ?, ?);", (name, x, y))
-
+    # Grab all data
     result = bdb.execute(
-        "SELECT * FROM t"
-    )
+        "SELECT * FROM t")
+    # Convert to json
     print conv_cursor_to_json(result)
+    # Clean up
+    bdb.execute("DROP TABLE t")
