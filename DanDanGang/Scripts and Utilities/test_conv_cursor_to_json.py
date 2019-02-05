5a8867022f9374fa1ee627750f636b7eed48a5bb
Dan <djh242@cam.ac.uk>
2019-02-05 19:10:56 +0000

    minor fixes to conv_cursor_to_json

--- DanDanGang/Scripts and Utilities/test_conv_cursor_to_json.py
+++ DanDanGang/Scripts and Utilities/test_conv_cursor_to_json.py
@@ -15,7 +15,6 @@ with bayeslite.bayesdb_open(pathname=db_pathname) as bdb:
         "SELECT * FROM t")
     # Convert to json
     result = conv_cursor_to_json(result)
-    print result
-    print result == json.dumps([{"y": 11, "x": 1, "name": "A"}, {"y": 12, "x": 2, "name": "B"}, {"y": 13, "x": 3, "name": "C"}])
+    assert result == json.dumps([{"y": 11, "x": 1, "name": "A"}, {"y": 12, "x": 2, "name": "B"}, {"y": 13, "x": 3, "name": "C"}])
     # Clean up
     bdb.execute("DROP TABLE t")
