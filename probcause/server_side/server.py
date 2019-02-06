b5f38f4b5afc4f73f36cc949ed7c47d18e21c85e
Dan <djh242@cam.ac.uk>
2019-02-06 20:07:49 +0000

    wrote server.py, may not work

--- probcause/server_side/server.py
+++ probcause/server_side/server.py
@@ -1,48 +1,91 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import bayeslite
+from bayeslite import BQLError, BQLParseError, BayesDBException, BayesDBTxnError
+from probcause.util.conv_cursor_to_json import conv_cursor_to_json
+import json
 
-
-escape_char = '\n'
-'''
-Assuming requests are of the form:
-    "foo.bdb
-    QUERY_1
-    QUERY_2
+"""
+REQUESTS
+Requests are 'text/http' and are in the following format:
+    database_name.bdb
+    query1
+    query2
     ...
-    QUERY_N"
-'''
+    queryN
+Note that the queries should not contain new lines. 
+
+RESPONSES
+Responses are 'text/http' in the case that an error occurs:
+    error_information
+    
+Or 'application/json' in the normal case:
+    [
+        {response1 },
+        {response2 },
+        ...
+        {response3}
+    ]
+"""
 
 
 class RequestHandler(BaseHTTPRequestHandler):
+    @staticmethod
+    def text_to_queries(text):
+        t = text.split("\n")
+        assert len(t) > 1
+        db_name = t[0]
+        queries = t[1:]
+        return db_name, queries
+
+    @staticmethod
+    def results_to_json(results):
+        assert len(results > 0)
+        return json.dumps(results)
+
     def do_POST(self):
         self.send_response(200)
 
-        content_length = int(self.headers['Content-Length'])
-        request = self.rfile.read(content_length).decode("utf8").split(escape_char)
+        err = False
 
-        client_ip = self.client_address[0]
-        print "Received query from: "+str(client_ip)
+        try:
+            content_length = int(self.headers['Content-Length'])
+            text = self.rfile.read(content_length).decode("utf8")
+            db_name, queries = RequestHandler.text_to_queries(text)
+        except UnicodeDecodeError:
+            err = "Error: Couldn't decode request, make sure it's utf8"
+        except AssertionError:
+            err = "Error: You must give at least 1 query."
 
-        db_pathname = request[0]
-        queries = request[1:]
+        if not err:
+            results = []
+            with bayeslite.bayesdb_open(pathname=db_name) as bdb:
+                for query in queries:
+                    try:
+                        results.append(conv_cursor_to_json(bdb.execute(query)))
+                    except (BQLError, BQLParseError, BayesDBException), e:
+                        err = e
+                        break
 
-        with bayeslite.bayesdb_open(pathname=db_pathname) as bdb:
-            for i, query in enumerate(queries[:-1]):
-                print "Executing query "+str(i)
-                bdb.execute(query)
-            print "Executing final query"
-            result = bdb.execute(queries[-1])
+        if not err:
+            self.send_header('Content-type', 'text/http')
+            self.end_headers()
+            message = RequestHandler.results_to_json(results)
+        else:
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            message = str(err)
 
-        self.send_header('Content-type', 'text/http')
-        self.end_headers()
-        message = "Can't fill your request (not implemented)"
-        self.wfile.write(bytes(message))
-
-
-def run_server(port, ip):
-    httpd = HTTPServer((ip, port), RequestHandler)
-    httpd.serve_forever()
+        self.wfile.write(message)
 
 
+def main(ip, port):
+    try:
+        httpd = HTTPServer((ip, port), RequestHandler)
+        print "Running server..."
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print "Stopping server."
 
 
+if __name__ == '__main__':
+    main('', 80)
