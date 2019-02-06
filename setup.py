27d035119e830862254668d2fa26ae2d63d3cc0c
Dan <djh242@cam.ac.uk>
2019-02-06 14:58:34 +0000

    readying the project for pip

--- setup.py
+++ setup.py
@@ -3,23 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name='probcause',
     version='1.0',
-    packages=[  # Names of packages
-        'probcause.client_side',
-        'probcause.server_side',
-        'probcause.demos',
-        'probcause.tests',
-        'probcause.util',
-    ],
-    package_dir={  # File paths
-        'probcause.client_side': 'client_side',
-        'probcause.server_side': 'server_side',
-        'probcause.demos': 'demos',
-        'probcause.tests': 'tests',
-        'probcause.util': 'util',
-    },
-    package_data={  # Data
-        'probcause.tests': [
-            'wc_forecasts.csv'
-        ],
-    },
+    packages=find_packages()
 )
