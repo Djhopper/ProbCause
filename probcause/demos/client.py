5141f90a8372bfddc5b1db534f19b9317f0df3a2
Dan <djh242@cam.ac.uk>
2019-02-08 17:37:15 +0000

    test

--- probcause/demos/client.py
+++ probcause/demos/client.py
@@ -2,12 +2,13 @@ import httplib
 
 
 def run(ip, port):
+    print 1
     # make connection
     connection = httplib.HTTPConnection(ip, port)
-
+    print 2
     # make request
     connection.request("POST", "/", raw_input("Please input your name:\n"))
-
+    print 3
     # get response
     response = connection.getresponse()
     print(response.read().decode("utf-8"))
