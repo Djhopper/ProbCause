fbe5c99b1bd3561f61b3f867d053700fe6e3a03a
Dan <djh242@cam.ac.uk>
2019-02-08 17:35:11 +0000

    testing whether can do POST over port 8082 successfully

---
+++ probcause/tests/test_server_demo.py
@@ -0,0 +1,13 @@
+import probcause.demos.server as server
+import probcause.demos.client as client
+import threading
+
+ip = '128.232.98.213'
+port = 8082
+
+
+server_thread = threading.Thread(target=server.run(port, ip))
+server_thread.daemon = True
+server_thread.start()
+
+client.run(ip, port)
