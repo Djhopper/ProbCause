322714dec969a60fe0b11140218c149f109b8570
dks28 <dks28@cam.ac.uk>
2019-02-09 12:13:39 +0000

    Fix most of the send_request tests.

--- probcause/tests/test_send_request.py
+++ probcause/tests/test_send_request.py
@@ -6,6 +6,8 @@ import json
 import time
 import os
 import subprocess
+import pytest
+import sys
 
 '''
 This script tests the functionality of probcause.client_side.send_request. That is,
@@ -53,9 +55,9 @@ def test_lines_to_queries():
     output_files = ['test_queries/' + f for f in os.listdir('test_queries')]
     for i, f in enumerate(test_files):
         print('Checking queryfication of ' + f)
-        lines = open(file=f, mode='r').readlines()
+        lines = open(name=f, mode='r').readlines()
         queries = send_request.lines_to_queries(lines)
-        assert queries == open(file=output_files[i], mode='r').readlines()
+        assert queries == [s.replace('\n', '') for s in open(name=output_files[i], mode='r').readlines()]
 
 
 def test_no_arguments():
@@ -63,13 +65,12 @@ def test_no_arguments():
     server_thread.daemon = True
     server_thread.start()
     time.sleep(0.5)  # XXX Bad way to ensure the server is definitely done setting up
-
-    process = subprocess.Popen(['python', '../client_side/send_request.py'],
-                               stdout=subprocess.PIPE)
-    
-    response = (process.returncode, process.communicate())
-    expected = (2, ['Please provide either a file to read the query/queries from, or a string containing the query.', 'Usage: send_request.py --db=<DB> [--file=<FILE>] [--query=<QUERY>]. Please provide at least one of the bracketed options.'])
-
+    sys.argv = ['send_request.py']
+    with pytest.raises(send_request.BadOptionsError) as e:
+    	send_request.main()
+        
+    response = (e.value.value, e.value.message)
+    expected = (3, 'Please provide the db file to operate on.')
     assert response == expected
 
 
@@ -78,12 +79,12 @@ def test_bad_options():
     server_thread.daemon = True
     server_thread.start()
     time.sleep(0.5)  # XXX Bad way to ensure the server is definitely done setting up
+    sys.argv = ['send_request.py', '--badoption']
+    with pytest.raises(send_request.BadOptionsError) as e:
+        send_request.main() 
     
-    process = subprocess.Popen(['python', '../client_side/send_request.py', '--badoption'], 
-        stdout=subprocess.PIPE)
-    
-    response = process.returncode
-    expected = 1
+    response = (e.value.value, e.value.message)
+    expected = (1, 'Bad options given.')
 
     assert response == expected
 
@@ -93,12 +94,12 @@ def test_no_db():
     server_thread.daemon = True
     server_thread.start()
     time.sleep(0.5)  # XXX Bad way to ensure the server is definitely done setting up
+    sys.argv = ['send_request.py', '--file=../tests/test_queries/1'] 
+    with pytest.raises(send_request.BadOptionsError) as e:
+        send_request.main() 
     
-    process = subprocess.Popen(['python', '../client_side/send_request.py', '--file=../tests/test_queries/1'], 
-        stdout=subprocess.PIPE)
-    
-    response = process.returncode
-    expected = 3
+    response = (e.value.value, e.value.message)
+    expected = (3, 'Please provide the db file to operate on.')
 
     assert response == expected
 
@@ -108,14 +109,14 @@ def test_no_query():
     server_thread.daemon = True
     server_thread.start()
     time.sleep(0.5)  # XXX Bad way to ensure the server is definitely done setting up
+    expected = (2, 'Please provide either a file to read the query/queries from, or a string containing the query. Usage: send_request.py --db=<DB> [--file=<FILE>] [--query=<QUERY>]. Please provide at least one of the bracketed options.') 
+    sys.argv = ['send_request.py', '--db=foo.bdb']
+    with pytest.raises(send_request.BadOptionsError) as e:
+        send_request.main()
     
-    process = subprocess.Popen(['python', '../client_side/send_request.py', '--db=foo.db'], 
-        stdout=subprocess.PIPE)
-    
-    response = process.returncode
-    expected = 2
+    response = (e.value.value, e.value.message)
 
-    assert response == expected
+    assert expected == response 
 
 
 def test_no_error_query_given():
@@ -124,25 +125,28 @@ def test_no_error_query_given():
     server_thread.start()
     time.sleep(0.5)  # XXX Bad way to ensure the server is definitely done setting up
     
-    process = subprocess.Popen(['python', '../client_side/send_request.py', '--db=foo.db', '--query=\'DROP TABLE t;\''], 
-        stdout=subprocess.PIPE)
-    
-    response = process.returncode
-    expected = 0
+    success = True
+    sys.argv = ['send_request.py', '--db=foo.db', '--query=\'DROP TABLE t;\'']
+    try:
+        send_request.main()
+    except Exception as e:
+        success = False
 
-    assert response == expected
+    assert success
 
 
 def test_no_error_file_given():
+    '''
     server_thread = threading.Thread(target=server_thread_function, args=())
     server_thread.daemon = True
     server_thread.start()
     time.sleep(0.5)  # XXX Bad way to ensure the server is definitely done setting up
-    
-    process = subprocess.Popen(['python', '../client_side/send_request.py', '--db=foo.db', '--file=../tests/test_queries/1'], 
-        stdout=subprocess.PIPE)
-    
-    response = process.returncode
-    expected = 0
+    '''
+    success = True
+    sys.argv = ['send_request.py', '--db=foo.db', '--file=../tests/test_queries/1']
+    try:
+        send_request.main()
+    except Exception as e:
+        success = False
 
-    assert response == expected
+    assert success 
