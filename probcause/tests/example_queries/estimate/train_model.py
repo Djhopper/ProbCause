83abf5cb2b7a753b891bfaec8a5bab07613f8927
Dan <djh242@cam.ac.uk>
2019-02-11 13:38:45 +0000

    add __main__

--- probcause/tests/example_queries/estimate/train_model.py
+++ probcause/tests/example_queries/estimate/train_model.py
@@ -2,8 +2,13 @@ from estimate_setup import db_handle
 import bayeslite
 
 
-with bayeslite.bayesdb_open(pathname=db_handle) as bdb:
-    iterations = int(input("How many iterations to do: "))
-    print "Training model..."
-    bdb.execute(
-        "ANALYZE g FOR " + str(iterations) + " ITERATIONS")
+def main():
+    with bayeslite.bayesdb_open(pathname=db_handle) as bdb:
+        iterations = int(input("How many iterations to do: "))
+        print "Training model..."
+        bdb.execute(
+            "ANALYZE g FOR " + str(iterations) + " ITERATIONS")
+
+
+if __name__ == "__main__":
+    main()
