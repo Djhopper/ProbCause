cbee789b9954b0198fe0005c727c130fafeb0279
Dan <djh242@cam.ac.uk>
2019-03-04 13:26:40 +0000

    add manual schema for live database

--- probcause/resources/live_environment/bdb_setup.py
+++ probcause/resources/live_environment/bdb_setup.py
@@ -5,25 +5,24 @@ db_handle = 'african_traffic_data.bdb'
 table_name = 'TRAFFICDATA'
 csv_handle = '../datasets/traffic_data.csv'
 
+
 def main():
     with bayeslite.bayesdb_open(pathname=db_handle) as bdb:
         with open(csv_handle, 'r') as f:
             table_from_csv(bdb, table_name, f)
 
-        ignore = ['CaseNumber', 'Date', 'Block', 'IUCR', 'PrimaryType', 'Description',
-                  'LocationDescription', 'FBICode', 'UpdatedOn', 'XCoordinate',
-                  'YCoordinate', 'Latitude', 'Longitude', 'Location']
-        numerical = ['ID', 'Beat', 'District', 'Year']
-        nominal = ['Arrest', 'Domestic', 'CommunityArea', 'Ward']
-	'''
+        ignore = ["date", "time"]
+        numerical = ["distance_to_nearest_traffic_light", "speed_limit", "estimated_speed_of_collision"]
+        nominal = ["vehicle_type", "second_vehicle_type", "road_has_pavement", "distance_to_nearest_traffic_light",
+                   "seat_belt_used", "injury_sustained", "lethal", "land_use", "city"]
+
         schema = \
             "(" + \
             "; ".join("ignore "+i for i in ignore) + "; " + \
             "; ".join(i + " numerical " for i in numerical) + "; " + \
             "; ".join(i + " nominal" for i in nominal) + \
             ")"
-	'''
-	schema = '(GUESS STATTYPES OF (*))'
+
         print "Creating population..."
         bdb.execute(
             "CREATE POPULATION FOR " + table_name + " " + schema)
